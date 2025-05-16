9516c7f1c1ffdb4ac88906494cae817a8a72f7b6
Santiago <santy15021502@gmail.com>
2025-05-16 08:49:50 -0300

    inicio

---
+++ parcial-1-div132-main/Parcial/js/main.js
@@ -0,0 +1,90 @@
+/*  
+    Instrucciones del Parcial
+
+    - Responde los puntos en orden.
+    - Se valorará:
+        * Código limpio
+        * Comentarios claros
+        * Separación en bloques funcionales
+        * Buen uso de funciones/modularización
+
+    IMPORTANTE:
+    - El trabajo debe desarrollarse utilizando buenas prácticas de programación en JavaScript.
+*/
+
+/*  
+    Punto 1 _________________________
+
+    Este parcial consiste en crear el frontend de una tienda de frutas.
+    Para ello ya se dispone del HTML y deberás programar el JavaScript necesario.
+
+    1. Almacena tus datos personales (nombre, apellido, DNI) en un objeto y:
+        - Imprime tu nombre y apellido en la etiqueta del <nav> (donde corresponda).
+        - Imprímelo también en la consola.
+*/
+
+/*  
+    Punto 2 _________________________
+
+    Simula la carga de datos desde un archivo `db.json`. Este debe tener objetos con esta estructura:
+    {
+        "id": 1,
+        "nombre": "arandano",
+        "precio": 5000,
+        "img": "img/arandano.jpg"
+    }
+*/
+
+/*  
+    Punto 3 _________________________
+
+    Imprime los productos en pantalla al cargar la página.
+    Agrega esta funcionalidad dentro de la función `init()`.
+
+    El HTML que debes agregar por cada producto es el siguiente:
+
+        <div class="product-card">
+            <img src="ruta" alt="nombre">
+            <h3>Nombre del producto</h3>
+            <p>$Precio</p>
+            <button class="add-to-cart">Agregar a carrito</button>
+        </div>
+*/
+
+/*  
+    Punto 4 _________________________
+
+    Crea la función `filtro()` para filtrar los productos por nombre.
+    - Asocia esta función al evento `keyup` de un campo `<input>`.
+    - Cada vez que se escriba una letra, deben mostrarse solo los productos que coincidan con el texto ingresado.
+*/
+
+/*  
+    Punto 5 _________________________
+
+    Agrega la funcionalidad de carrito:
+    - Crea un array `carrito` que almacene los productos seleccionados.
+    - Al presionar “Agregar a carrito”, el producto debe aparecer en el listado con id `cart-items`.
+
+    El HTML del carrito debe tener el siguiente formato:
+
+        <li class="item-block">
+            <p class="item-name">nombreproducto - $precioproducto</p>
+            <button class="delete-button">Eliminar</button>
+        </li>
+*/
+
+/*  
+    Punto 6 _________________________
+
+    Guarda los productos del carrito en `localStorage`.
+    - Asegúrate de que al recargar la página el carrito se recupere automáticamente desde `localStorage`.
+*/
+
+// Función inicializadora
+function init() {
+  // Aquí deben invocarse todas las funciones necesarias para que la aplicación comience a funcionar
+  
+}
+
+
